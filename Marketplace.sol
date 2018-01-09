6dd5277d9a82c792de05941a02c5a5903e365b5e
GitKilla <robertnclark1@gmail.com>
2018-01-08 19:31:53 -0500

    Update Marketplace.sol

--- Marketplace.sol
+++ Marketplace.sol
@@ -11,19 +11,25 @@ contract ModelMarketplace {
     }
     
     Bid[] liveBids;
-    mapping(address => Bid[]) bidmap;
+    /*mapping(address => Bid[]) bidmap;*/
     event LogNewBid(Bid newBid);
     
-    function addBid (uint tarVar, uint price, address bidAddress string[][] trainingData, string[][] testingData, uint index) 
+    function addBid (uint tarVar, uint price, address bidAddress, string[][] trainingData, string[][] testingData, uint index) 
                                                                                                             returns (bool success) {
         Bid lot = new Bid(tarVar, price, bidAddress, aTrainingData, aTestingData, index);
         liveBids.push(lot);
-        bidmap[address].push(lot);
+        /*bidmap[address].push(lot);*/
         LogNewBid(lot);
     }
     
     function removeBid(address aClient, index anIndex) returns (bool success) {
-    
-
-    
+        for(unit i = 0; i < 100 /*ONLY ALLOWS 100 BIDS AT A TIME*/; i++) {
+            address tempAddress = liveBids[i].client;
+            unit tempIndex = liveBids[i].client
+            if(tempAddress == aClient && tempIndex == anIndex) {
+                liveBids[i] = liveBids[liveBids.length-1];
+            }
+        }
+        return true;
+     }
 }
